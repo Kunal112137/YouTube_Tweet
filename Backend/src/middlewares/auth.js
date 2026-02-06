be84454dad7cb65588b30dfe0a695fb9000f87aa
kunal chavhan <153000419+Kunal112137@users.noreply.github.com>
2026-02-06 04:50:36 -0500

    Update auth.js

--- Backend/src/middlewares/auth.js
+++ Backend/src/middlewares/auth.js
@@ -3,7 +3,7 @@
 import  { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
-import { ApiErrors } from "../utils/apiErrors.js";
+import { ApiErrors } from "../utils/ApiErrors.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
